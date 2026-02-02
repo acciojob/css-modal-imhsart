eca278a0960964acf90f41d959613f6a121011e0
Nitin Rana <nitinrana2519@gmail.com>
2026-02-02 21:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let modal = document.querySelector('.modal')
+let openBtn = document.querySelector('#openModal')
+let closeBtn = document.querySelector('.close-modal')
+let modcontent = document.querySelector('.modal-content')
+openBtn.addEventListener('click', () => {
+	modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none';
+})
+modal.addEventListener('mousedown', (e) => {
+	if(!modcontent.contains(e.target)){
+		modal.style.display = 'none';
+	}
+})
